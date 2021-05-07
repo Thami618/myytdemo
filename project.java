de209a381d3fbd95d0ea560b82f1febcf62d6f4d
Thami618 <thamngcobo1998@gmail.com>
2021-05-07 12:02:41 +0200

    init

---
+++ project.java
@@ -0,0 +1,16 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        int passmark = 12;
+        boolean passed = false;
+        int[] scores = {4,6,2,8,12,35,9};
+        for (int unitScore : scores){
+            if(unitScore >= 12){
+                passed = true;
+                break;
+            }
+        }
+        System.out.println("At the "+ passed);
+    }
+    
+}
